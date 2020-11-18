51909692d3cae742703cc15079d748d85c3cc945
Kanakanti Balakrishnareddy <55091139+balakrishnareddy246@users.noreply.github.com>
2020-11-18 06:57:26 +0530

    Create arithmetic.go

---
+++ arithmetic.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+         "fmt"
+)
+
+func main () {
+    
+    a :=10 
+    b :=3
+    fmt.Println(a+b)
+    fmt.Println(a-b)
+    fmt.Println(a*b)
+    fmt.Println(a/b)
+    fmt.Println(a%b)
+
+}
