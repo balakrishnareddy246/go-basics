06e3ab8c118d892a930a9e2b434fc8f65cc16975
Kanakanti Balakrishnareddy <55091139+balakrishnareddy246@users.noreply.github.com>
2020-11-20 07:18:52 +0530

    Create Subdevideof.go

---
+++ Subdevideof.go
@@ -0,0 +1,29 @@
+package main 
+
+import  "fmt"
+
+func main () {
+    
+    i:=1
+    for i<=10 {
+        
+        fmt.Println(i)
+        i++
+    }
+}
+ 
+ 
+ OUT PUT
+ 
+ go man.go
+ 
+ 1
+ 2
+ 3
+ 4
+ 5
+ 6
+ 7
+ 8
+ 9
+ 10
