acd0ac98078dcf4fcb2a495b1da04b3ba255e44b
Kanakanti Balakrishnareddy <55091139+balakrishnareddy246@users.noreply.github.com>
2020-11-20 08:00:25 +0530

    Create VariableDeclared.go

---
+++ VariableDeclared.go
@@ -0,0 +1,17 @@
+package main 
+
+import "fmt"
+
+func main () {
+    var x float64
+    x = 20.0
+    fmt.Println(x)
+    fmt.Println("x is of type %T\n",x)
+    
+}
+
+
+
+out put
+go run main.go
+20 x is type float64
