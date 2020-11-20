c7c4fbe260ee5a481f59e0f9ba3f783722665e48
Kanakanti Balakrishnareddy <55091139+balakrishnareddy246@users.noreply.github.com>
2020-11-20 06:13:44 +0530

    Create 1Variable.go

---
+++ 1Variable.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main () {
+    
+    var a int  = 5
+   var b float32 = 4.54
+   const pi float64 = 3.1415131975
+   x,y := 14,15
+   fmt.Println(a)
+   fmt.Println(b)
+   fmt.Println(pi)
+   fmt.Println(x,",",y)
+}
+
+
+
+
+out put
+
+5
+4.54
+3.1415131975
+14 , 15
