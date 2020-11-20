bd0d156b2acd0c1ab52297dd493a2d766b1af6f8
Kanakanti Balakrishnareddy <55091139+balakrishnareddy246@users.noreply.github.com>
2020-11-20 07:24:48 +0530

    Create Nestedloop.go

---
+++ Nestedloop.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func main () {
+    
+    for i:=1 ; i<10; i++ {
+        
+        for j:=1; j<i; j++{
+            fmt.Println("*Balakrishnareddy")
+        }
+   fmt.Println()
+}
+}
+OUT PUT 
+$go run main.go
+
+*Balakrishnareddy
+
+*Balakrishnareddy
+*Balakrishnareddy
+
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
+*Balakrishnareddy
